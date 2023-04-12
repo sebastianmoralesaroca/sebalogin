d1f60fe32c43cdf433f287d07ed433371bd81fd6
seba.developer <cmoralesaroca@gmail.com>
2023-04-11 21:46:58 -0400

    verificar la logitud de los password

--- src/script.js
+++ src/script.js
@@ -1,39 +1,82 @@
+var contPrincipal = document.getElementById('contPrincipal');
 var cont1 = document.getElementById('cont1');
 var cont2 = document.getElementById('cont2');
 var signIn = document.getElementById('signIn');
 var signUp = document.getElementById('signUp');
-signIn.addEventListener('click', function () {
-    var userIn = document.createElement('input');
+var userIn = document.createElement('input');
+var passwordIn = document.createElement('input');
+var btnIn = document.createElement('button');
+function funSignIn() {
     userIn.id = 'userIn';
     userIn.type = 'text';
     userIn.placeholder = "User Name";
     cont2.appendChild(userIn);
-    var passwordIn = document.createElement('input');
     passwordIn.id = 'passwordIn';
     passwordIn.type = 'password';
     passwordIn.placeholder = "Password";
     cont2.appendChild(passwordIn);
-    var btnIn = document.createElement('button');
     btnIn.id = 'btnIn';
     btnIn.type = 'submit';
     btnIn.innerHTML = "SIGN IN";
     cont2.appendChild(btnIn);
-    btnIn.addEventListener('click', function () {
-        if (userIn.value === "seba") {
-            alert("hola seba !");
+}
+var userUp = document.createElement('input');
+var passwordUp1 = document.createElement('input');
+var passwordUp2 = document.createElement('input');
+var btnUp = document.createElement('button');
+function funSignUp() {
+    userUp.id = 'userUp';
+    userUp.type = 'text';
+    userUp.placeholder = "Create Name User";
+    cont2 === null || cont2 === void 0 ? void 0 : cont2.appendChild(userUp);
+    passwordUp1.id = 'passwordUp1';
+    passwordUp1.type = 'password';
+    passwordUp1.placeholder = "Create Password";
+    cont2 === null || cont2 === void 0 ? void 0 : cont2.appendChild(passwordUp1);
+    passwordUp2.id = 'passwordUp2';
+    passwordUp2.type = 'password';
+    passwordUp2.placeholder = "Repeat Password";
+    cont2.appendChild(passwordUp2);
+    btnUp.id = 'btnUp';
+    btnUp.type = 'submit';
+    btnUp.innerHTML = "SIGN UP";
+    cont2 === null || cont2 === void 0 ? void 0 : cont2.appendChild(btnUp);
+}
+funSignUp();
+btnUp.addEventListener('click', function () {
+    if (passwordUp1.value === passwordUp2.value) {
+        alert("Hola " + userUp.value + ", su usuario ha sido creado!");
+    }
+    else if (passwordUp1.value.length < 4) {
+        alert("Necesitas un Password");
+    }
+    else {
+        alert("Los Password No Coinciden");
+    }
+});
+signIn.addEventListener('click', function () {
+    cont2.removeChild(userUp);
+    cont2.removeChild(passwordUp1);
+    cont2.removeChild(passwordUp2);
+    cont2.removeChild(btnUp);
+    funSignIn();
+    console.log("Click Sign In");
+    btnIn.addEventListener("click", function () {
+        if (userIn.value === userUp.value) {
+            alert(userIn.value);
         }
         else {
-            alert("tu no eres seba !");
+            alert("error");
         }
     });
 });
-function funSignUp() {
-    var passwordUp2 = document.createElement('input');
-    passwordUp2.type = 'text';
-    passwordUp2.id = 'repeatPassword';
-    passwordUp2.placeholder = "Repeat Password";
-    cont2.appendChild(passwordUp2);
-}
+signUp.addEventListener('click', function () {
+    cont2.removeChild(userIn);
+    cont2.removeChild(passwordIn);
+    cont2.removeChild(btnIn);
+    funSignUp();
+    console.log("Click Sign Up");
+});
 /*
 function funAutentications () {
     if (userIn === "seba" || passwordIn === "1") {
--- src/script.ts
+++ src/script.ts
@@ -5,55 +5,52 @@ const signIn = document.getElementById('signIn') ;
 const signUp = document.getElementById('signUp') ;
 
 
+
+const userIn = document.createElement('input') ;
+const passwordIn = document.createElement('input') ;
+const btnIn = document.createElement('button') ;
+
+
 function funSignIn () {
-    const userIn = document.createElement('input') ;
     userIn.id = 'userIn' ;
     userIn.type = 'text' ;
     userIn.placeholder = "User Name" ;
     cont2.appendChild( userIn ) ;
 
-    const passwordIn = document.createElement('input') ;
     passwordIn.id = 'passwordIn' ;
     passwordIn.type = 'password' ;
     passwordIn.placeholder = "Password" ;
     cont2.appendChild( passwordIn ) ;
 
-    const btnIn = document.createElement('button') ;
     btnIn.id = 'btnIn' ;
     btnIn.type = 'submit' ;
     btnIn.innerHTML = "SIGN IN" ;
     cont2.appendChild(btnIn) ;
-
-    btnIn.addEventListener ( 'click', () => {
-        if (userIn.value === "seba" || passwordIn.value === 1) {
-            alert("hola seba !") ;
-        } else {
-            alert("tu no eres seba !") ;
-        }
-    })
 }
 
 
+const userUp = document.createElement('input') ;
+const passwordUp1 = document.createElement('input') ;
+const passwordUp2 = document.createElement('input') ;
+const btnUp = document.createElement('button') ;
+
+
 function funSignUp () {
-    const userUp = document.createElement('input') ;
     userUp.id = 'userUp' ;
     userUp.type = 'text' ;
     userUp.placeholder = "Create Name User" ;
     cont2?.appendChild(userUp) ;
 
-    const passwordUp1 = document.createElement('input') ;
     passwordUp1.id = 'passwordUp1' ;
     passwordUp1.type = 'password' ;
     passwordUp1.placeholder = "Create Password"
     cont2?.appendChild(passwordUp1) ;
 
-    const passwordUp2 = document.createElement('input') ;
     passwordUp2.id = 'passwordUp2' ;
     passwordUp2.type = 'password' ;
     passwordUp2.placeholder = "Repeat Password" ;
     cont2.appendChild(passwordUp2) ;
 
-    const btnUp = document.createElement('button') ;
     btnUp.id = 'btnUp' ;
     btnUp.type = 'submit' ;
     btnUp.innerHTML = "SIGN UP" ;
@@ -62,8 +59,16 @@ function funSignUp () {
 }
 
 
-
 funSignUp() ;
+btnUp.addEventListener('click', () => {
+    if (passwordUp1.value === passwordUp2.value) {
+        alert("Hola " + userUp.value + ", su usuario ha sido creado!");
+    } else if (passwordUp1.value.length < 4) {
+        alert("Necesitas un Password")
+    } else {
+        alert("Los Password No Coinciden") ;
+    }
+})
 
 
 signIn.addEventListener('click', () => {
@@ -74,7 +79,16 @@ signIn.addEventListener('click', () => {
     
     funSignIn() ;
     console.log("Click Sign In") ;
+
+    btnIn.addEventListener("click", () => {
+        if (userIn.value === userUp.value ) {
+            alert(userIn.value) ;
+        } else {
+            alert("error");
+        }
+    })
 })
+
 
 
 signUp.addEventListener ('click', () => {
